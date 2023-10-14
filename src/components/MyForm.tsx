4f055644d514371fce459347a796dc818412da7b
Cristiano Ap Lazaro <cristianoalazaro@hotmail.com>
2023-10-14 11:30:57 -0300

    implementing Control

--- src/components/MyForm.tsx
+++ src/components/MyForm.tsx
@@ -1,4 +1,4 @@
-import { useForm, useFieldArray } from "react-hook-form";
+import { useForm, useFieldArray, Controller } from "react-hook-form";
 import * as Yup from 'yup';
 import { yupResolver } from '@hookform/resolvers/yup';
 import Input from "./Input";
@@ -13,6 +13,7 @@ type FormType = {
     name: string;
     lastName?: string;
     email: string;
+    age?: number;
     password: string;
     phoneNumbers?: {
         number: string;
@@ -23,7 +24,7 @@ const simulateRequest = async () => {
     const myPromise = new Promise((resolve) => {
         setTimeout(() => {
             resolve('Hello');
-        },2000);
+        }, 2000);
     });
 
     return myPromise;
@@ -57,18 +58,18 @@ function MyForm() {
         <form onSubmit={handleSubmit(handleSubmitData)}>
             <div className="mb-3">
                 <label htmlFor="name" className="form-label">Nome</label>
-                <input 
-                    {...register('name')} 
-                    type="text" 
-                    className={`form-control ${errors.name ? 'border-danger':'border-success'}`} 
+                <input
+                    {...register('name')}
+                    type="text"
+                    className={`form-control ${errors.name ? 'border-danger' : 'border-success'}`}
                     id="name"
-                    autoFocus 
+                    autoFocus
                 />
-                {errors.name && <p style={{color: 'red'}}>{errors.name.message}</p>}
+                {errors.name && <p style={{ color: 'red' }}>{errors.name.message}</p>}
             </div>
             <div className="mb-3">
                 <label className="form-label" htmlFor="lastName">Sobrenome</label>
-                <Input 
+                <Input
                     {...register('lastName')}
                     className="form-control border-success"
                     id="lastName"
@@ -76,18 +77,31 @@ function MyForm() {
                 />
             </div>
             <div className="mb-3">
+                <Controller
+                    control={control}
+                    name="age"
+                    render={({ field: { onChange, onBlur, value, ref } }) => (
+                        <input type='number'
+                            onChange={onChange}
+                            onBlur={() => {alert(value)}}
+                            value={value}
+                        />
+                    )}
+                />
+            </div>
+            <div className="mb-3">
                 <label htmlFor="email" className="form-label">E-mail</label>
-                <input {...register('email')} type="email" className={`form-control ${errors.email ? 'border-danger':'border-success'}`} id="email" />
-                {errors.email && <p style={{color: 'red'}}>{errors.email.message}</p>}
+                <input {...register('email')} type="email" className={`form-control ${errors.email ? 'border-danger' : 'border-success'}`} id="email" />
+                {errors.email && <p style={{ color: 'red' }}>{errors.email.message}</p>}
             </div>
             <div className="mb-3">
                 <label>List of phone number</label>
                 <div>
                     {fields.map((field, index) => {
                         return (<div className="form-control d-flex" key={field.id}>
-                            <input 
-                                type="text" {...register(`phoneNumbers.${index}.number` as const)} 
-                                className="form-control border-success"    
+                            <input
+                                type="text" {...register(`phoneNumbers.${index}.number` as const)}
+                                className="form-control border-success"
                             />
                             {
                                 index >= 0 && (
@@ -97,14 +111,15 @@ function MyForm() {
                                 )
                             }
                         </div>
-                    )})}
-                    <button className="btn btn-secondary" onClick={() => append({number: ""})}>Add Phone Number</button>
+                        )
+                    })}
+                    <button className="btn btn-secondary" onClick={() => append({ number: "" })}>Add Phone Number</button>
                 </div>
             </div>
             <div className="mb-3">
                 <label className="form-label" htmlFor="password">Senha</label>
-                <input {...register('password')} type="password" className={`form-control ${errors.password ? 'border-danger':'border-success'}`} id="password" />
-                {errors.password && <p style={{color: 'red'}}>{errors.password.message}</p>}
+                <input {...register('password')} type="password" className={`form-control ${errors.password ? 'border-danger' : 'border-success'}`} id="password" />
+                {errors.password && <p style={{ color: 'red' }}>{errors.password.message}</p>}
             </div>
             <button type="submit" className="btn btn-primary" disabled={isSubmitting}>
                 {`${isSubmitting ? 'Enviando...' : 'Gravar'} `}
